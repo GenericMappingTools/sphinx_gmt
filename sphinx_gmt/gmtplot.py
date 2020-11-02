64bc8d04aee9447a50a62539992db6862c150887
Dongdong Tian <seisman.info@gmail.com>
2020-11-02 15:37:56 -0500

    Fix pylint issues

--- sphinx_gmt/gmtplot.py
+++ sphinx_gmt/gmtplot.py
@@ -145,8 +145,8 @@ def _search_images(cwd):
             raise ValueError("More than one figure generated in one GMT plot.")
         elif len(ps_images) == 1:  # PS found
             cmd = "gmt psconvert -A -P -T{} {}"
-            subprocess.run(cmd.format("g", ps_images[0]), shell=True)
-            subprocess.run(cmd.format("f", ps_images[0]), shell=True)
+            subprocess.run(cmd.format("g", ps_images[0]), shell=True, check=False)
+            subprocess.run(cmd.format("f", ps_images[0]), shell=True, check=False)
             png_images = list(cwd.glob("*.png"))
             pdf_images = list(cwd.glob("*.pdf"))
 
@@ -177,6 +177,7 @@ def eval_bash(code, code_dir, output_dir, output_base):
         proc = subprocess.run(
             "bash {}".format(Path(tmpdir, "script.sh")),
             shell=True,
+            check=False,
             cwd=tmpdir,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
