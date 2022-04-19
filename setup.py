8889b8c6f13f589a256619b01fea9d16697b263c
Dongdong Tian <seisman.info@gmail.com>
2022-04-19 20:24:08 +0800

    Bump the minimum required Python version to 3.7

--- setup.py
+++ setup.py
@@ -32,12 +32,14 @@ CLASSIFIERS = [
     "Intended Audience :: Education",
     "Topic :: Scientific/Engineering",
     "Topic :: Software Development :: Libraries",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "License :: OSI Approved :: {}".format(LICENSE),
 ]
 PLATFORMS = "Any"
-PYTHON_REQUIRES = ">=3.6"
+PYTHON_REQUIRES = ">=3.7"
 INSTALL_REQUIRES = ["jinja2", "sphinx"]
 
 if __name__ == "__main__":
